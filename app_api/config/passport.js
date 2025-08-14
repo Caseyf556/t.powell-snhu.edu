4d125e782340bf75bb82373e98f1c90e76c9b865
Caseyf556 <154101111+Caseyf556@users.noreply.github.com>
2025-08-14 11:43:36 -0500

    Update passport.js

--- app_api/config/passport.js
+++ app_api/config/passport.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 const User = mongoose.model("users");
 
-// Passport to use email as the username field
+// Uses Passport to use email as the username field
 passport.use(
   new LocalStrategy(
     { usernameField: "email" },
@@ -13,12 +13,14 @@ passport.use(
         const user = await User.findOne({ email });
 
         if (!user) {
-          return done(null, false, { message: "Email not recognized." });
+          return done(null, false, { 
+            message: "Email not recognized." });
         }
 
         const isValid = user.validatePassword(password);
         if (!isValid) {
-          return done(null, false, { message: "Invalid password." });
+          return done(null, false, { 
+            message: "Invalid password." });
         }
 
         return done(null, user);
