566478ecc007adda223c813e4d77e6b197233a15
Caseyf556 <154101111+Caseyf556@users.noreply.github.com>
2025-08-14 11:50:25 -0500

    Update index.js

--- travlr/routes/index.js
+++ travlr/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const controller = require('../controllers/main');
 
-/* Home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
+router.get('/', controller.index);
 module.exports = router;
