14e04867e5b4e1fa436e73ba01bb4edb398a28ee
Caseyf556 <154101111+Caseyf556@users.noreply.github.com>
2025-08-14 10:27:22 -0500

    Update index.js

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,23 +1,30 @@
-const express = require('express');
-const router = express.Router();
-const authRouter = require('./authentication');
-const blogsRouter = require('./blog-posts');
-const latestRouter = require('./latest-posts');
-const mealsRouter = require('./meal-posts');
-const newsRouter = require('./news-posts');
-const roomsRouter = require('./rooms');
-const testimonialsRouter = require('./testimonials');
-const tripsRouter = require('./trips');
-const tipsRouter = require('./vacation-tips');
+const express = require("express");
+const { Router } = express;
 
-router.use('/auth', authRouter);
-router.use('/blogs', blogsRouter);
-router.use('/latest', latestRouter);
-router.use('/meals', mealsRouter);
-router.use('/news', newsRouter);
-router.use('/rooms', roomsRouter);
-router.use('/testimonials', testimonialsRouter);
-router.use('/trips', tripsRouter);
-router.use('/tips', tipsRouter);
+const jwt = require("express-jwt");
+const { login, register } = require("../controllers/authentication");
+const {
+  tripsList,
+  tripsAddTrip,
+  tripsFindCode,
+  tripsUpdateTrip,
+} = require("../controllers/trips");
+
+const router = Router();
+const requireAuth = jwt({
+  secret: process.env.JWT_SECRET,
+  algorithms: ["HS256"],
+  userProperty: "payload",
+});
+
+router.post("/login", login);
+router.post("/register", register);
+router.get("/trips", tripsList);
+router.post("/trips", requireAuth, tripsAddTrip);
+
+router
+  .route("/trips/:tripCode")
+  .get(tripsFindCode)
+  .put(requireAuth, tripsUpdateTrip);
 
 module.exports = router;
