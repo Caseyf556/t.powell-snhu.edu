287a642e32e1b4937671bfc93733ae3bc698d0a2
Caseyf556 <154101111+Caseyf556@users.noreply.github.com>
2025-08-14 11:09:00 -0500

    Update Trip.js

--- Data/models/Trip.js
+++ Data/models/Trip.js
@@ -1,25 +1,23 @@
-const mongoose = require('mongoose');
-
+const mongoose = require('mongoose')
+// define trip
 const tripSchema = new mongoose.Schema({
-  destination: {
-    type: String,
-    required: true,
-  },
-  duration: {
-    type: Number,
-    required: true,
-    min: 1,
-  },
-  price: {
-    type: Number,
-    required: true,
-    min: 0,
-  },
-  startDate: {
-    type: Date,
-    required: true,
-  },
-  description: String,
+    
+  code: {type: String, required: true, index: true },
+  
+    name: {type: String, required: true, index: true},
+  
+    length: {type: String, required: true},
+  
+    start: {type: Date, required: true},
+    resort: {type: String, required: true},
+  
+    perPerson: {type: String, required: true},
+  
+    image: {type: String, required: true},
+  
+    description: {type: String, required: true}
+  
 });
 
-module.exports = mongoose.model('Trip', tripSchema);
+// Trip Model("trips", tripSchema): 
+module.exports = mongoose.model("trips", tripSchema);
